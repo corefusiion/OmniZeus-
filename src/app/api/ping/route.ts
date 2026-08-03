b81e3c2c8515167b357542972c4d80ba35fcf96b
Gleisson de Jesus Santos <T034183@embasanet.ba.gov.br>
2026-08-03 15:29:39 -0300

    test: add isolated ping route to verify next-on-pages runtime

---
+++ src/app/api/ping/route.ts
@@ -0,0 +1,10 @@
+export const runtime = "edge";
+
+export async function GET() {
+  return new Response("pong", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain"
+    }
+  });
+}
